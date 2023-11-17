fa6ddd073824b56833965271c37214e73b9e900a
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2023-11-17 23:20:13 +0000

    Correct handling of optional format tests
    fixes: #79
    
    Most implementations, especially older ones, tend not to offer control over format assertions.  However, a few did and correcting the test harness to only enable format assertions for tests under `optional/format` did improve their results.

--- src/main/java/org/creekservice/kafka/test/perf/implementations/EveritImplementation.java
+++ src/main/java/org/creekservice/kafka/test/perf/implementations/EveritImplementation.java
@@ -70,7 +70,15 @@ public class EveritImplementation implements Implementation {
 
     @Override
     public JsonValidator prepare(
-            final String schema, final SchemaSpec spec, final AdditionalSchemas additionalSchemas) {
+            final String schema,
+            final SchemaSpec spec,
+            final AdditionalSchemas additionalSchemas,
+            final boolean enableFormatAssertions) {
+
+        /*
+        Implementation does not seem to currently provide a way to programmatically turn on format assertions.
+        Instead, they seem to be on-by-default, which is not inline with the draft 2020-12 spec.
+         */
 
         final Object schemaObject = parse(schema);
 
